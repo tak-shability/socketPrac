e5df3aa506bcc2d71d8f40b6ed0e56706f6fb1c3
tak-shability <tak@shability.io>
2022-05-17 16:22:56 +0900

    test7

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ io.on('connection', (socket) => {
         // for (let i = 0; i < data.pcb.length; i++) {
         //     data.pcb[i].numb = Number(data.pcb[i].numb)
         // }
-        // data.pcb.map(v => Number(v.numb))
-        // data.pcb.sort((a, b) => a.numb-b.numb)
+        data.pcb.map(v => Number(v.numb))
+        data.pcb.sort((a, b) => a.numb-b.numb)
         for (let i=0; i < data.pcb.length; i++) {
-            console.log(`전력 정보 ${i+1}번 포트: ${JSON.stringify(data.pcb)}`)
+            console.log(`전력 정보 ${i+1}번 포트: ${JSON.stringify(data.pcb[i])}`)
         }
         
     })
