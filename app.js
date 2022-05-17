73561eec576d2feec37fd268adbcf6e38806373d
tak-shability <tak@shability.io>
2022-05-17 13:01:26 +0900

    test

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const socket = require('socket.io');
+const io = socket(http)
+const port = 3000;
+
+io.on('connection', (socket) => {
+    console.log(`커넥션 이벤트 발생, 소켓 아이디: ${socket.id}`)
+
+    socket.on('login', (user) => {
+        console.log(`로그인 이벤트 발생, 로그인 타입: ${user.type}, 아이디: ${user.id}, 소켓 아이디: ${socket.id}`)
+    
+    socket.on('insert', (data) => {
+        if (typeof(data) !== 'object') data = JSON.parse(data)
+        console.log(data)
+    })
+    })
+});
+
+http.listen(port, () => {
+    console.log('서버가 연결되었습니다.');
+});
