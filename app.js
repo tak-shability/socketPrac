a5c6f5e3be0f2fa40e0b78255e0c059b2ef42502
tak-shability <tak@shability.io>
2022-05-17 13:41:16 +0900

    test1

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ io.on('connection', (socket) => {
     console.log(`커넥션 이벤트 발생, 소켓 아이디: ${socket.id}`)
 
     socket.on('login', (user) => {
+        console.log('user', user)
         console.log(`로그인 이벤트 발생, 로그인 타입: ${user.type}, 아이디: ${user.id}, 소켓 아이디: ${socket.id}`)
     
     socket.on('insert', (data) => {
