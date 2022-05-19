62017a030597d6653598176246984b1beba61542
tak-shability <tak@shability.io>
2022-05-19 10:47:22 +0900

    test

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use('/', (req, res) => {
 });
 
 let userList = [];
+let insertData = '';
 
 io.on('connection', (socket) => {
     console.log(`커넥션 이벤트 발생\n소켓 아이디: ${socket.id}`);
@@ -51,9 +52,8 @@ io.on('connection', (socket) => {
         data.pcb.sort((a, b) => a.numb - b.numb);
         function insertData() {
             for (let i = 0; i < data.pcb.length; i++) {
-                console.log(`전력 정보 ${i + 1}번 포트: ${JSON.stringify(data.pcb[i])}`);
+                return (insertData += `전력 정보 ${i + 1}번 포트: ${JSON.stringify(data.pcb[i])}`);
             }
-            return;
         }
 
         // insertData();
