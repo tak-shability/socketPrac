ff53ee6d2e8bc45d2278415a88580545f5fece7a
tak-shability <tak@shability.io>
2022-05-19 10:43:23 +0900

    test

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ io.on('connection', (socket) => {
             for (let i = 0; i < data.pcb.length; i++) {
                 console.log(`전력 정보 ${i + 1}번 포트: ${JSON.stringify(data.pcb[i])}`);
             }
+            return;
         }
 
         // insertData();
