71737ae2cdd1dc79187155f33af5db847d49b8ae
tak-shability <tak@shability.io>
2022-05-19 16:41:17 +0900

    test

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ io.on('connection', (socket) => {
             console.log('portData.station_id', portData.station_id);
             console.log('portData', portData);
             if (userList[i].userName === portData.station_id) {
-                io.to(userList[i].socketID).emit('port_ready', usedData);
+                io.to(userList[i].socketID).emit('port_ready', portData);
                 break;
             }
         }
